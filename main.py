185cc078a3cc28dfe4e64be28ec4c7094301120a
Wijgey <167464970+Wijgey@users.noreply.github.com>
2024-05-23 21:05:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+import discord
+from discord.ext import commands
+import random
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("BYEEEEE!!!")
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+@bot.command()
+async def mem(ctx):
+    files = os.listdir('images')
+    
+    if files:
+        img_name = random.choice(files)
+        
+        with open(f'images/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+            
+        await ctx.send(file=picture)
+    else:
+        await ctx.send('Resimler klasörü boş!')
+
+bot.run(" ")
